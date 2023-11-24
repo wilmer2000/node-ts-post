cb8fb002f7c910783fc3b9d2c64c32779792df6e
Wilmer Blanco <wilmervzla2000@gmail.com>
2023-11-24 18:13:58 -0300

    progres

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,13 +1,34 @@
-import express = require('express');
-import bodyParser = require('body-parser');
-import routes from './routes/post.route';
+import { Schema, model, connect } from 'mongoose';
 
-const app = express();
-const port = 3000;
+// 1. Create an interface representing a document in MongoDB.
+interface IUser {
+    name: string;
+    email: string;
+    avatar?: string;
+}
 
-app.use(bodyParser.json());
-app.use('/api', routes);
-
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// 2. Create a Schema corresponding to the document interface.
+const userSchema = new Schema<IUser>({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    avatar: String
 });
+
+// 3. Create a Model.
+const User = model<IUser>('User', userSchema);
+
+run().catch(err => console.log(err));
+
+async function run() {
+    // 4. Connect to MongoDB
+    await connect('mongodb://db:27017');
+
+    const user = new User({
+        name: 'Bill',
+        email: 'bill@initech.com',
+        avatar: 'https://i.imgur.com/dM7Thhn.png'
+    });
+    await user.save();
+
+    console.log(user.email); // 'bill@initech.com'
+}
